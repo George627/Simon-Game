9a751cb6ce75f1afb51e0d2b0500690d7a15b794
George627 <81629113+George627@users.noreply.github.com>
2023-12-11 22:34:29 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,18 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+
+const gamePattern = [];
+
+const nextSequence = () => {
+    
+    const randomNumber = Math.floor(Math.random() * 4);
+
+    const randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+    
+
+}
+
